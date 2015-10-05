21388f78ba6de8d83b180c1ea93971fc07470eb9
zhangyunfan <zhangyunfan@bytedance.com>
2015-10-05 15:43:45 +0800

    First version
    Change-Id: Ib5ab50d46526494467059abb2ccba2d8a6f649d0

--- Answer/template/problem.js
+++ Answer/template/problem.js
@@ -7,23 +7,61 @@ function getTextGroundId(s) {
     return '#P' + s + '-text';
 }
 
+function changeCorrectAnswerColor(correct) {
+    var txtName = getTextGroundId(correct);
+    var txt = $(txtName)[0];
+
+    $(txt).addClass(' correct-answer ')
+}
+
+function showFlag() {
+    $($('#flag')[0]).removeClass('hidden')
+}
+
+function showCorrectResult() {
+    $($('#correctResult')[0]).removeClass('hidden')
+}
+
+function showWaResult() {
+    $($('#errorResult')[0]).removeClass('hidden');
+    $($('#errorText')[0]).removeClass('hidden');
+}
+
+function jump2next() {
+    window.location = '/participant/problem?name=' + commonInfo.next + '&correct=' + commonInfo.correctNum;
+}
+
 function clickChoices(s) {
+    if (commonInfo.isAnswered) {
+        jump2next();
+        return;
+    }
+
+    commonInfo.isAnswered = true;
     var bgName = getBackgroundId(s);
     var bg = $(bgName)[0];
-//    $(bg)[0].src = 'normal.png';
-    //sleep?
     bg.src = '/static/normal.png';
 
     var textName = getTextGroundId(s);
-    var text = $(textName)[0];
-    if (s === correctAnswer) {
-        ;
+    var text = $($(textName)[0]);
+    if (s !== commonInfo.correctAnswer) {
+        text.addClass(' failed-answer ');
+        showWaResult();
     } else {
-
+        commonInfo.correctNum += 1;
+        showCorrectResult();
     }
+
+    changeCorrectAnswerColor(commonInfo.correctAnswer);
+    showFlag();
 }
-
-
+var clickCount = 0;
+$('body').click(function() {
+    if (clickCount > 0 && commonInfo.isAnswered) {
+        jump2next();
+    }
+    clickCount += 1;
+});
 
 //$('#PAText')[0].outerText = choiceTextMap['A'];
 
--- Participant/views.py
+++ Participant/views.py
@@ -6,20 +6,59 @@ from models import Participant
 from django.shortcuts import render_to_response
 # Create your views here.
 
-problem_info = {
+problem_infoMap = {
     '1': {
+        'id': 1,
         'A': 'A: 上海奥迪国际赛车场',
         'B': 'B: 珠海国际赛车场',
         'C': 'C: 北京京港国际赛车场',
-        'correct': 'A'
+        'correct': 'A',
+        'next': '2'
     },
+    '2': {
+        'id': 2,
+        'A': 'A: 青年冠军方程式系列赛',
+        'B': 'B: 兰博基尼-宝珀<br>Super Trofeo亚洲挑战赛',
+        'C': 'C: 北京京港国际赛车场',
+        'correct': 'A',
+        'next': '3'
+    },
+    '3': {
+        'id': 3,
+        'A': 'A: 1个回合',
+        'B': 'B: 2个回合',
+        'C': 'C: 3个回合',
+        'correct': 'C',
+        'next': '4'
+    },
+    '4': {
+        'id': 4,
+        'A': 'A: Gallardo',
+        'B': 'B: Hurac',
+        'C': 'C: 北京京港国际赛车场',
+        'correct': 'C',
+        'next': '5'
+    },
+    '5': {
+        'id': 5,
+        'A': 'A: 上海奥迪国际赛车场',
+        'B': 'B: 珠海国际赛车场',
+        'C': 'C: 北京京港国际赛车场',
+        'correct': 'B',
+        'next': 'result'
+    },
+    'result': {
+        'id': 'result'
+    }
 }
+
 
 def add_participant(request):
     name = request.GET.get('name')
     address = request.GET.get('address')
     phone = request.GET.get('phone')
     return HttpResponse('')
+
 
 def list_participant(request):
     pass
@@ -28,6 +67,20 @@ def list_participant(request):
 def get_problem_html(request):
     name = request.GET.get('name')
     correct = request.GET.get('correct')
-    return render_to_response("template/problem-2.html", Context(dict(problem_info=problem_info.get(name),
-                                                                      correct=correct)))
-
+    if correct is None or name is None:
+        return HttpResponse('Failed.')
+    problem_info = problem_infoMap.get(name)
+    correct_num = int(correct)
+    if not problem_info:
+        return HttpResponse('Failed.')
+    if name != 'result':
+        return render_to_response("template/problemJpg.html", Context(dict(problem_info=problem_info,
+                                                                           correctNum=correct,
+                                                                           waInfo=problem_info[problem_info['correct']])))
+    else:
+        if correct_num < 2:
+            return render_to_response("template/title0.html", Context(dict()))
+        elif correct_num < 5:
+            return render_to_response("template/title1.html", Context(dict()))
+        else:
+            return render_to_response("template/title2.html", Context(dict()))
