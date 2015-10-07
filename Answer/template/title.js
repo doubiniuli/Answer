c0c8eaeb2b378f07555c4b611a05e9e865a0c622
lijun <lijun@wandoujia.com>
2015-10-08 00:50:36 +0800

    fixed share

--- Answer/template/title.js
+++ Answer/template/title.js
@@ -29,25 +29,48 @@ $('body').click(function (e) {
     if (isAcceptButton(x, y)) {
         window.location.href="/static/submit.html";
     } else if (isShareButton(x, y)) {
-        window.location.href="http://www.cnblogs.com";
     }
 });
 
-var appId = "wx87b100a1972be176";
-var url = "http://192.168.1.101:8000/Answer/template/title1.html";
+var appId = "wx6ee8795bbaee6f5d";
+var url = document.location.href;
 var title = "赛道学霸挑战赛，你想参与吗？";
 var desc = "";
-var link = "http://192.168.1.101:8000/Answer/template/title1.html";
-var imgUrl = "";
+var link = "http://www.zhaopg.com/Public/images/11.png";
+var imgUrl = "http://www.zhaopg.com/Public/images/11.png";
 
-var token = "9fuw8eua98fua9w8ef9a";
+
+$.ajax({
+	type: 'GET',
+	url: 'http://test.agenda-bj.com.cn:8082/api/GetJsShareScript?appid=' + appid + '&url=' + encodeURIComponent(url),
+	dataType: 'jsonp',
+	success: function (data) {
+		console.log(data);
+		wx.config({
+			debug: false,
+			appId: appid,
+			timestamp: data.timestamp,
+			nonceStr: data.noncestr,
+			signature: data.signature,
+			jsApiList: [
+				'checkJsApi',
+				'onMenuShareTimeline',
+				'onMenuShareAppMessage',
+				'onMenuShareQQ',
+				'onMenuShareWeibo',
+				'hideMenuItems',
+				'showMenuItems'
+			]
+		});
+	}
+})
 
 wx.config({
     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: appId, // 必填，公众号的唯一标识
-    timestamp: "1444027314", // 必填，生成签名的时间戳
-    nonceStr: "oz9QQXKChBDwzys", // 必填，生成签名的随机串
-    signature: "9feb41c4fbf561b53422757372e5d1720e83cbbc",// 必填，签名，见附录1
+    timestamp: "1444234760", // 必填，生成签名的时间戳
+    nonceStr: "PgVVTUUZHNmcqkg", // 必填，生成签名的随机串
+    signature: "d3d25493007fab98b32e137ecb227ffc3c04f482",// 必填，签名，见附录1
     jsApiList: [
         'checkJsApi',
         'onMenuShareTimeline',
