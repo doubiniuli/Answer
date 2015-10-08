27c177f92d8063eabde2fff0e9dbfb684ccefac0
zhangyunfan <zhangyunfan@bytedance.com>
2015-10-08 11:42:04 +0800

    Fix
    Change-Id: I0a305d7f06dd36ed5e26723ad21fc7f46b7dda99

--- Answer/template/title.js
+++ Answer/template/title.js
@@ -27,17 +27,18 @@ $('body').click(function (e) {
     var x = e.pageX / width;
     var y = e.pageY / height;
 
+    if (showSharePng) {
+        showSharePng = false;
+        $($("#share_jpg")[0]).addClass("hidden")
+        return
+    }
+
     if (isAcceptButton(x, y)) {
         window.location.href="/static/submit.html";
     } else if (isShareButton(x, y)) {
         if (!showSharePng) {
             showSharePng = true;
             $($("#share_jpg")[0]).removeClass("hidden")
-        }
-    } else {
-        if (showSharePng) {
-            showSharePng = false;
-            $($("#share_jpg")[0]).addClass("hidden")
         }
     }
 });
