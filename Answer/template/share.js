7be7010fb2c770932b5aefa9bfb672aa61e8df9f
zhangyunfan <zhangyunfan@bytedance.com>
2015-10-11 23:34:22 +0800

    Modify share
    Change-Id: I63139e90ac911230e98fa0dae4d734e0f421b918

---
+++ Answer/template/share.js
@@ -0,0 +1,104 @@
+
+//var appid = "wx6ee8795bbaee6f5d";
+var appid = "wx704bcfcf388ec118";
+var url = document.location.href;
+var titleFinal = title;
+var desc = "";
+var link = "http://othergame.yaohehe.com/static/index.html";
+var imgUrl = "http://othergame.yaohehe.com/static/small-img/index.jpg";
+
+
+$.ajax({
+	type: 'GET',
+	url: '/participant/weixin?app_id=' + appid + '&url=' + encodeURIComponent(url),
+	dataType: 'jsonp',
+	success: function (data) {
+		console.log(data);
+		wx.config({
+			debug: false,
+			appId: appid,
+			timestamp: data.timestamp,
+			nonceStr: data.noncestr,
+			signature: data.signature,
+			jsApiList: [
+				'checkJsApi',
+				'onMenuShareTimeline',
+				'onMenuShareAppMessage',
+				'onMenuShareQQ',
+				'onMenuShareWeibo',
+				'hideMenuItems',
+				'showMenuItems'
+			]
+		});
+	}
+})
+
+wx.ready(function(){
+
+wx.onMenuShareTimeline({
+    title: titleFinal, // 分享标题
+    link: link, // 分享链接
+    imgUrl: imgUrl, // 分享图标
+    success: function () {
+        // 用户确认分享后执行的回调函数
+    },
+    cancel: function () {
+        // 用户取消分享后执行的回调函数
+    }
+});
+
+wx.onMenuShareAppMessage({
+    title: titleFinal, // 分享标题
+    desc: desc, // 分享描述
+    link: link, // 分享链接
+    imgUrl: imgUrl, // 分享图标
+    type: '', // 分享类型,music、video或link，不填默认为link
+    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+    success: function () {
+        // 用户确认分享后执行的回调函数
+    },
+    cancel: function () {
+        // 用户取消分享后执行的回调函数
+    }
+});
+
+wx.onMenuShareQQ({
+    title: titleFinal, // 分享标题
+    desc: desc, // 分享描述
+    link: link, // 分享链接
+    imgUrl: imgUrl, // 分享图标
+    success: function () {
+       // 用户确认分享后执行的回调函数
+    },
+    cancel: function () {
+       // 用户取消分享后执行的回调函数
+    }
+});
+
+
+wx.onMenuShareWeibo({
+    title: titleFinal, // 分享标题
+    desc: desc, // 分享描述
+    link: link, // 分享链接
+    imgUrl: imgUrl, // 分享图标
+    success: function () {
+       // 用户确认分享后执行的回调函数
+    },
+    cancel: function () {
+        // 用户取消分享后执行的回调函数
+    }
+});
+
+wx.onMenuShareQZone({
+    title: titleFinal, // 分享标题
+    desc: desc, // 分享描述
+    link: link, // 分享链接
+    imgUrl: imgUrl, // 分享图标
+    success: function () {
+       // 用户确认分享后执行的回调函数
+    },
+    cancel: function () {
+        // 用户取消分享后执行的回调函数
+    }
+});
+});
